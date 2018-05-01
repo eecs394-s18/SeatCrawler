6847c530d2a4dcab9d13ec811df98b90deba6e36
Ben Krege <jbkrege@gmail.com>
2018-05-01 00:36:37 -0500

    added color changing algorithm to slider, but it seems like Ionic doesn't support updating colors to something that isn't in the .scss file which bites

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -3,7 +3,7 @@ import { NavController, NavParams, Platform} from 'ionic-angular';
 import {Cafe} from "../../app/cafe";
 import {AngularFireDatabase, AngularFireObject} from 'angularfire2/database';
 import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator';
-
+// import * as ColorMath from "color-math"
 /**
  * Generated class for the DetailsPage page.
  *
@@ -115,6 +115,64 @@ export class DetailsPage {
     //var busyness = {'busyness': {'0': [timestamp, ratio*100] }}
     //this.cafes.update(busyness)
   }
+
+  changeGradient2(ratio: number){
+    // red:    #ff0000,
+    // yellow: #ffff00,
+    // green:  #008000
+    if (ratio == 0.5){
+      this.item.color = "#ffff00";
+    }
+
+    else if(ratio < 0.5){
+      var r = ratio/0.5;
+      var r2 = 1-r;
+
+      var red = 255*r; // #ff
+      var green = 255*r + 128*r2; // #ff*r + #80*r2
+      var blue = 0;
+
+      this.item.color = '0x'+red.toString(16).substring(0,2)+green.toString(16).substring(0,2)+"00";
+      console.log("Slider color changed to "+this.item.color);
+    }
+    else {
+      var r2 = 1-((ratio-.5)/.5);
+
+      var green = 255*r2
+
+      this.item.color = '0xff'+green.toString(16).substring(0,2)+"00";
+      console.log("Slider color changed to "+this.item.color);
+    }
+  //var busyness = {'busyness': {'0': [timestamp, ratio*100] }}
+  //this.cafes.update(busyness)
+  }
+
+  // changeGradientWithColorMath(ratio: number){
+  //   // red:    #ff0000,
+  //   // yellow: #ffff00,
+  //   // green:  #008000
+
+  //   if (ratio == 0.5){
+  //     this.item.color = "yellow";
+  //   }
+  //   else if(ratio < 0.5){
+  //     var r = String(ratio/.5);
+  //     var r2 = String(1-(ratio/.5));
+  //     console.log(ColorMath.evaluate(r+" * #ffff00"))
+  //     var ex = r+"* #ffff00 + "+ r2+" * #008000";
+  //     console.log(ex);
+  //     this.item.color = ColorMath.evaluate(ex).result.resultStr;
+  //     console.log(this.item.color);
+  //   }
+  //   else {
+  //     var r = String((ratio-.5)/.5);
+  //     var r2 = String(1-Number(r));
+  //     this.item.color = ColorMath.evaluate(r+"* #ff0000 + "+ r2 +" * #ffff00").result.hex();
+  //     console.log(this.item.color);
+  //   }
+  //   //var busyness = {'busyness': {'0': [timestamp, ratio*100] }}
+  //   //this.cafes.update(busyness)
+  // }
 
 
   updateDataOfDay():void {
